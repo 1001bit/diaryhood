6c667b452eb2bf1b2cf1a32cde0f476b25fd5fc2
Schalmanoff <yaribakal@gmail.com>
2024-12-14 18:30:40 +0300

    improved user service tests readability

--- services/user/test/test.go
+++ services/user/test/test.go
@@ -13,104 +13,123 @@ import (
 )
 
 func testServer(t *testing.T, ctx context.Context, server *server.Server, emailChan <-chan *rabbitemail.EmailBody) {
-	// Send Bad Login
-	_, err := server.SendOtpEmail(ctx, &userpb.SendOtpEmailRequest{Login: "unknown"})
-	if status.Code(err) != codes.NotFound {
-		t.Fatal(err)
-	}
+	respEmail := ""
+	t.Run("Request otp email", func(t *testing.T) {
+		// Send Bad Login
+		_, err := server.SendOtpEmail(ctx, &userpb.SendOtpEmailRequest{Login: "unknown"})
+		if status.Code(err) != codes.NotFound {
+			t.Fatal(err)
+		}
 
-	// Send Good Login
-	resp, err := server.SendOtpEmail(ctx, &userpb.SendOtpEmailRequest{Login: "good@email.com"})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Catch emailBody from Consumer
-	emailBody := <-emailChan
-	if resp.Email != emailBody.Email {
-		t.Fatal("bad email:", emailBody, resp.Email)
-	}
-
-	// Login with OTP from emailBody
-	// Bad inputs
-	_, err = server.VerifyOtp(ctx, &userpb.VerifyOtpRequest{
-		Email: "bad@email.com",
-		Otp:   emailBody.Otp,
+		// Send Good Login
+		resp, err := server.SendOtpEmail(ctx, &userpb.SendOtpEmailRequest{Login: "good@email.com"})
+		if err != nil {
+			t.Fatal(err)
+		}
+		respEmail = resp.Email
 	})
-	if status.Code(err) != codes.NotFound {
-		t.Fatal("expected not found, but got:", err)
-	}
 
-	_, err = server.VerifyOtp(ctx, &userpb.VerifyOtpRequest{
-		Email: emailBody.Email,
-		Otp:   "wrongotp",
+	emailOtp := ""
+	t.Run("receive email", func(t *testing.T) {
+		// Wait for email
+		emailBody := <-emailChan
+		if respEmail != emailBody.Email {
+			t.Fatalf("expected %s, but got %s", respEmail, emailBody.Email)
+		}
+		emailOtp = emailBody.Otp
 	})
-	if status.Code(err) != codes.NotFound {
-		t.Fatal("expected not found, but got:", err)
-	}
 
-	// Good input
-	tokens, err := server.VerifyOtp(ctx, &userpb.VerifyOtpRequest{
-		Email: emailBody.Email,
-		Otp:   emailBody.Otp,
+	var tokens *userpb.TokensResponse = nil
+	t.Run("login with otp", func(t *testing.T) {
+		// Bad inputs
+		_, err := server.VerifyOtp(ctx, &userpb.VerifyOtpRequest{
+			Email: "bad@email.com",
+			Otp:   emailOtp,
+		})
+		if status.Code(err) != codes.NotFound {
+			t.Fatal("expected not found, but got:", err)
+		}
+
+		_, err = server.VerifyOtp(ctx, &userpb.VerifyOtpRequest{
+			Email: respEmail,
+			Otp:   "wrongotp",
+		})
+		if status.Code(err) != codes.NotFound {
+			t.Fatal("expected not found, but got:", err)
+		}
+
+		// Good input
+		tokens, err = server.VerifyOtp(ctx, &userpb.VerifyOtpRequest{
+			Email: respEmail,
+			Otp:   emailOtp,
+		})
+		if err != nil {
+			t.Fatal("expected OK, but got:", err)
+		}
 	})
-	if err != nil {
-		t.Fatal("expected OK, but got:", err)
-	}
 
-	// Get profile
 	claims, ok := accesstoken.ExtractClaims(tokens.AccessJWT)
 	if !ok {
 		t.Fatal("expected OK, but not OK")
 	}
-	profile, err := server.GetProfile(ctx, &userpb.GetProfileRequest{Id: claims.Id})
-	if err != nil {
-		t.Fatal("expected OK, but got:", err)
-	}
 
-	t.Log("date:", profile.Date)
+	t.Run("get profile", func(t *testing.T) {
+		profile, err := server.GetProfile(ctx, &userpb.GetProfileRequest{Id: claims.Id})
+		if err != nil {
+			t.Fatal("expected OK, but got:", err)
+		}
 
-	// Rename user
-	_, err = server.ChangeUsername(ctx, &userpb.ChangeUsernameRequest{Id: claims.Id, NewName: "bad name"})
-	if status.Code(err) != codes.InvalidArgument {
-		t.Fatal("expected InvalidArgument, but got:", err)
-	}
+		t.Log("date:", profile.Date)
+	})
 
-	_, err = server.ChangeUsername(ctx, &userpb.ChangeUsernameRequest{Id: claims.Id, NewName: "toolongname111111111111111111111111111111"})
-	if status.Code(err) != codes.InvalidArgument {
-		t.Fatal("expected InvalidArgument, but got:", err)
-	}
+	t.Run("change username", func(t *testing.T) {
+		_, err := server.ChangeUsername(ctx, &userpb.ChangeUsernameRequest{Id: claims.Id, NewName: "bad name"})
+		if status.Code(err) != codes.InvalidArgument {
+			t.Fatal("expected InvalidArgument, but got:", err)
+		}
 
-	_, err = server.ChangeUsername(ctx, &userpb.ChangeUsernameRequest{Id: claims.Id, NewName: "newname"})
-	if err != nil {
-		t.Fatal("expected OK, but got:", err)
-	}
+		_, err = server.ChangeUsername(ctx, &userpb.ChangeUsernameRequest{Id: claims.Id, NewName: "toolongname111111111111111111111111111111"})
+		if status.Code(err) != codes.InvalidArgument {
+			t.Fatal("expected InvalidArgument, but got:", err)
+		}
 
-	// get profile again
-	profile, err = server.GetProfile(ctx, &userpb.GetProfileRequest{Id: claims.Id})
-	if err != nil {
-		t.Fatal("expected OK, but got:", err)
-	}
+		_, err = server.ChangeUsername(ctx, &userpb.ChangeUsernameRequest{Id: claims.Id, NewName: "newname"})
+		if err != nil {
+			t.Fatal("expected OK, but got:", err)
+		}
+	})
 
-	if profile.Name != "newname" {
-		t.Fatal("expected newname, but got:", profile.Name)
-	}
+	t.Run("get profile", func(t *testing.T) {
+		profile, err := server.GetProfile(ctx, &userpb.GetProfileRequest{Id: claims.Id})
+		if err != nil {
+			t.Fatal("expected OK, but got:", err)
+		}
 
-	// Refresh the tokens
-	tokens, err = server.RefreshTokens(ctx, &userpb.RefreshTokenRequest{RefreshUUID: tokens.RefreshUUID})
-	if err != nil {
-		t.Fatal("expected OK, but got:", err)
-	}
+		if profile.Name != "newname" {
+			t.Fatal("expected newname, but got:", profile.Name)
+		}
+	})
 
-	// Logout
-	_, err = server.Logout(ctx, &userpb.RefreshTokenRequest{RefreshUUID: tokens.RefreshUUID})
-	if err != nil {
-		t.Fatal("expected OK, but got:", err)
-	}
+	t.Run("refresh tokens", func(t *testing.T) {
+		var err error
+		tokens, err = server.RefreshTokens(ctx, &userpb.RefreshTokenRequest{RefreshUUID: tokens.RefreshUUID})
+		if err != nil {
+			t.Fatal("expected OK, but got:", err)
+		}
+	})
 
-	// Try to refresh
-	tokens, err = server.RefreshTokens(ctx, &userpb.RefreshTokenRequest{RefreshUUID: tokens.RefreshUUID})
-	if status.Code(err) != codes.Unauthenticated {
-		t.Fatal("expected unauthenticated, but got:", err)
-	}
+	t.Run("logout", func(t *testing.T) {
+		_, err := server.Logout(ctx, &userpb.RefreshTokenRequest{RefreshUUID: tokens.RefreshUUID})
+		if err != nil {
+			t.Fatal("expected OK, but got:", err)
+		}
+	})
+
+	t.Run("refresh tokens", func(t *testing.T) {
+		var err error
+		tokens, err = server.RefreshTokens(ctx, &userpb.RefreshTokenRequest{RefreshUUID: tokens.RefreshUUID})
+		if status.Code(err) != codes.Unauthenticated {
+			t.Fatal("expected unauthenticated, but got:", err)
+		}
+	})
 }
